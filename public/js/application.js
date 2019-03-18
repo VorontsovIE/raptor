bdf83c06e3193f88c0676cd200d9df65bd8e8ad2
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-03-18 11:41:51 +0300

    button to generate random submission name

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
   bsCustomFileInput.init();
+  $('#submission-name-button').click(function(e){
+    e.preventDefault();
+    $.getJSON('/random_submission_name', function(data){
+      $('#submission-name-input').val(data['name']);
+    });
+  })
 });
